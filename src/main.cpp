30d7bb567f89057acfd706831f356e26d3826bca
lucaxgomex <lucas.g.oliv.dev@gmail.com>
2020-12-31 19:30:13 -0300

    Create a function for the screen display.

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,46 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
-int main()
+void drawScreen() 
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(200, 200), "Welcome to the Genius Game!"); //resoluçao da tela
+    sf::CircleShape buttonShape(100.f); //forma 
+    buttonShape.setFillColor(sf::Color::Blue); //cor
 
     while (window.isOpen())
     {
         sf::Event event;
+
         while (window.pollEvent(event))
-        {
+        {   /*
             if (event.type == sf::Event::Closed)
                 window.close();
-        }
+            */
 
+           switch (event.type)
+           {
+                // window closed
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+
+                // key pressed
+                case sf::Event::MouseButtonPressed:
+                    std::cout << "Hello World" << std::endl;
+                    break;
+                // we don't process other types of events
+                default:
+                    break;
+            }
+        }
         window.clear();
-        window.draw(shape);
+        window.draw(buttonShape);
         window.display();
     }
+}
 
+int main(void)
+{
+    drawScreen();
     return 0;
 }
